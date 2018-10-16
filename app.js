9ae30ce11411701089fee3f9a0b0392176f1b655
Paula Suarez <pauladzuck@hotmail.com>
2018-10-16 05:22:20 -0500

    adding updates to .js file

--- app.js
+++ app.js
@@ -19,14 +19,65 @@ function setName() {
 
 //expected result: function will be hoisted first, then the function call back, name will display in console log.
 
-console.log('first step');
-let average = getAvg(100, 100);
-function getAvg(x, y) {
-    console.log('second step');
-    var result = (x + y) / 2;
-    console.log('third step');
-    console.log(result);
-    console.log('fourth step');
+//console.log('first step');
+//let average = getAvg(100, 100);
+//function getAvg(x, y) {
+// console.log('second step');
+//var result = (x + y) / 2;
+//console.log('third step');
+//console.log(result);
+//console.log('fourth step');
 
+//}
+
+
+let avg = findAvg(2, 2);
+console.log('2', avg);
+function findAvg(a, b) {
+    console.log('1');
+    var answer = (a + b) / 2;
+    return answer;
 }
+
+//expected result:  hoist function first then call function and store in a global variable
+
+
+let fruits = [
+    'bananas',
+    'strawberries',
+    'peaches'
+];
+
+
+//let favFruit;
+let leastFav = 'orange';
+
+function getFruit() {
+    let favFruit = fruits[1];
+    console.log(fruits[0]);
+
+
+    function getFav() {
+        console.log(favFruit);
+        //let leastFav = 'orange';
+
+    }
+    console.log(leastFav);
+    getFav();
+}
+
+getFruit();
+
+
+sayHi('Paula');
+
+function sayHi(name) {
+    console.log(`Hello, ${name}`);
+}
+
+
+
+
+
+
 
